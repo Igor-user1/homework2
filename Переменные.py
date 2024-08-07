fed68e7cd7bc7e1d70eb551476974950cf312210
Igor-user1 <xadeev.igor@icloud.com>
2024-08-07 08:16:54 +0300

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,5 @@
+quantity_done_homeworks = 12
+spend_ours= 1.5
+name_course = 'Python'
+time_on_task = 0.125
+print('Крус: ',name_course,',','всего задач: ',quantity_done_homeworks,',','затрачено часов: ',spend_ours,',','среднее время выполнения: ', time_on_task)
